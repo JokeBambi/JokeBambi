c3608891a4b6e671485f7303c27aff8b41168211
Bamber <107054942+JokeBambi@users.noreply.github.com>
2023-08-06 17:13:19 -0400

    js

--- warn.js
+++ warn.js
@@ -1,4 +1,4 @@
 // warning incase!!!!!
-warn("woah woah woah!")
-warn("if someone told you to copy and paste something here or somewhere dont! it may get you hacked!")
+console.warn("woah woah woah!")
+console.warn("if someone told you to copy and paste something here or somewhere dont! it may get you hacked!")
 // warning because this commonly happens
