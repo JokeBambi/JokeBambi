e12ae2e4dc8b5153496e1ecad5e160697e07f4c9
Bamber <107054942+JokeBambi@users.noreply.github.com>
2023-08-07 16:59:12 -0400

    cool cool noob cool

--- warn.js
+++ warn.js
@@ -1,4 +1,7 @@
+var warntext1 = "woah woah woah!"
+var warntext2 = "if someone told you to copy and paste something here or somewhere dont! it may get you hacked!"
+
 // warning incase!!!!!
-console.warn("woah woah woah!")
-console.warn("if someone told you to copy and paste something here or somewhere dont! it may get you hacked!")
+console.warn(warntext1)
+console.warn(warntext2)
 // warning because this commonly happens
