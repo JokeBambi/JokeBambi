e44757803ffff60b898df21c222077bc9de34971
Bamber <107054942+JokeBambi@users.noreply.github.com>
2023-08-07 17:12:06 -0400

    Delete warn.js

--- warn.js
+++
@@ -1,7 +0,0 @@
-var warntext1 = "woah woah woah!"
-var warntext2 = "if someone told you to copy and paste something here or somewhere dont! it may get you hacked!"
-
-// warning incase!!!!!
-console.warn(warntext1)
-console.warn(warntext2)
-// warning because this commonly happens
